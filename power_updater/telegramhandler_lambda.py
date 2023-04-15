64955b2b74562f0019c4995d034e1ba59b7bf647
Yusuf Mayet <jojo786@gmail.com>
2023-04-15 10:56:20 +0200

    Changed the TelegramCommandHandler Lambda to rather call a Step Functions workflow that does getStage,getSchedule and sendNotifications

--- power_updater/telegramhandler_lambda.py
+++ power_updater/telegramhandler_lambda.py
@@ -3,22 +3,31 @@ import asyncio
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import os
-import get_stage_lambda
-import get_schedule_lambda
-from notification_lambda import CheckSchedule
+import boto3
+
 
 TelegramBotToken = os.environ['TelegramBotToken']
 TelegramChatID = os.environ['TelegramChatID']
+TelegramScheduleCommandStateMachine = os.environ['TelegramScheduleCommandStateMachine']
 
 application = ApplicationBuilder().token(TelegramBotToken).build()
+
+stepfunctions = boto3.client('stepfunctions')
 
 async def health(update: Update, context: ContextTypes.DEFAULT_TYPE):
     print("Start health command")
     await context.bot.send_message(chat_id=update.effective_chat.id, text="Was-salaam")
 
 async def schedule(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    print("Start Schedule command")
-    CheckSchedule()
+    print("Start Schedule command - Step Functions")
+    try:
+        response = stepfunctions.start_execution(
+                stateMachineArn=TelegramScheduleCommandStateMachine, input='{ }')
+    except Exception as err:
+            print("Exception: TelegramScheduleCommandStateMachine")
+            print (err) 
+    else:
+        return response['executionArn']
 
 def lambda_handler(event, context):
     return asyncio.get_event_loop().run_until_complete(main(event, context))
