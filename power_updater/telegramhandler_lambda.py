0a74c925562e3ec243660d6a0fa3ecc4e6a8fee3
Yusuf Mayet <jojo786@gmail.com>
2025-03-08 15:47:56 +0200

    Verify webhook calls are from Telegram using X-Telegram-Bot-Api-Secret-Token header

--- power_updater/telegramhandler_lambda.py
+++ power_updater/telegramhandler_lambda.py
@@ -9,17 +9,17 @@ from aws_lambda_powertools.utilities import parameters
 StackName = os.environ['StackName']
 TelegramScheduleCommandStateMachine = os.environ['TelegramScheduleCommandStateMachine']
 
-# Initialize SSM client
+# Initialize boto3 clients
+stepfunctions = boto3.client('stepfunctions')
 ssm = boto3.client('ssm')
+
 # Get the Telegram bot token from Parameter Store
 ssm_provider = parameters.SSMProvider()
 TelegramBotToken = ssm_provider.get('/'+StackName+'/telegram/prod/bot_token', decrypt=True)
 TelegramChatID = ssm_provider.get('/'+StackName+'/telegram/prod/chat_id', decrypt=True)
-
+TelegramBotAPISecretToken = ssm_provider.get('/'+StackName+'/telegram/prod/api_secret_token', decrypt=True)
 
 application = ApplicationBuilder().token(TelegramBotToken).build()
-
-stepfunctions = boto3.client('stepfunctions')
 
 async def health(update: Update, context: ContextTypes.DEFAULT_TYPE):
     print("Start health command")
@@ -37,7 +37,20 @@ async def schedule(update: Update, context: ContextTypes.DEFAULT_TYPE):
         return response['executionArn']
 
 def lambda_handler(event, context):
+    print("Start lambda_handler")
+
+    # Check if secret token header exists and matches expected value
+    if 'headers' not in event or \
+       'X-Telegram-Bot-Api-Secret-Token' not in event['headers'] or \
+       event['headers']['X-Telegram-Bot-Api-Secret-Token'] != TelegramBotAPISecretToken:
+        print("Unauthorized - Telegram Secret not found")
+        return {
+            'statusCode': 401,
+            'body': 'Unauthorized'
+        }
+    
     return asyncio.get_event_loop().run_until_complete(main(event, context))
+
 
 async def main(event, context):
    
